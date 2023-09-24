1d000da3fe95dcb525063c48befea3c471e041bf
Ramiro Cejas <ramiro12341@hotmail.com>
2023-09-24 18:11:30 -0300

    Initial commit

---
+++ resources/sinErrores/r_syn_local_vars.java
@@ -0,0 +1,14 @@
+///[SinErrores]
+class Class {
+    int x;
+    static int y, z = 10; // y y z son atributos estáticos
+    static int x, y = 5;  // x y y son atributos estáticos
+    String nombre = "Juan";
+    float salario, bono = 1000.50;
+    int edad = 25;
+    boolean esEstudiante = true;
+    char letraA = 'A';
+    int numero = 42;
+    String mensaje = "Hola, mundo!";
+    boolean esVerdadero = false;
+}
